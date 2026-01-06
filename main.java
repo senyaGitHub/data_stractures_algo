52ecbb4208b8bd697640a22c1fcc72c963019112
senyagithub <knight.fo@yandex.ru>
2026-01-06 19:20:43 +0000

    init

--- main.java
+++ main.java
@@ -1,10 +1,7 @@
 package perds;
 
-import perds.simulation.SimulationEngine;
-
 public class Main {
     public static void main(String[] args) {
-        SimulationEngine engine = new SimulationEngine();
-        engine.run();
+        System.out.println("hello.");
     }
 }
---
+++ src/main.java
@@ -0,0 +1,10 @@
+package perds;
+
+import perds.simulation.SimulationEngine;
+
+public class Main {
+    public static void main(String[] args) {
+        SimulationEngine engine = new SimulationEngine();
+        engine.run();
+    }
+}
