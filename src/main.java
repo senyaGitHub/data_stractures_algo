4ae929f1bfa2d053251eb39d828d0bffbdfec32c
senyagithub <knight.fo@yandex.ru>
2026-01-06 23:42:26 +0000

    test

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ import graph.Graph;
 public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph();
-        System.out.println("starting...");
-    
+        System.out.println("starting2...");
+
 }
 }
